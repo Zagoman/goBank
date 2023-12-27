8f250d0f43075bf187b6b5b59896b260954888ae
LucasZago <lucaszago.ph@gmail.com>
2023-12-27 21:00:03 +0100

    feat: implement storage interface, and provide it to server

--- storage.go
+++ storage.go
@@ -3,7 +3,7 @@ package main
 import (
 	"database/sql"
 
-	_ "github.com/lib/pg"
+	_ "github.com/lib/pq"
 )
 
 type Storage interface {
@@ -18,5 +18,30 @@ type PostgresStore struct {
 }
 
 func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "user=postgres dbname=postgres password=gobank sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
 
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{
+		db: db,
+	}, nil
+}
+
+func (s *PostgresStore) CreateAccount(*Account) error {
+	return nil
+}
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
+}
+func (s *PostgresStore) UpdateAccount(*Account) error {
+	return nil
+}
+func (s *PostgresStore) GetAccountById(id int) (*Account, error) {
+	return nil, nil
 }
